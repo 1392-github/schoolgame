6db0476e02028953fc69b247684c7d79de358c52
1392-github <ujo1033@gmail.com>
2026-08-04 19:06:07 +0900

    refactor: MoveScene 스크립트를 TitleManager로 변경

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class MoveScene : MonoBehaviour
+public class TitleManager : MonoBehaviour
 {
     SaveBuffer sb;
     public SaveFile4 tutorialDefaultSave;
