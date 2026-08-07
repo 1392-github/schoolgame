107177d2f4e481135979c3209a55408be5c8dec0
1392-github <ujo1033@gmail.com>
2026-08-07 20:22:43 +0900

    feat: 집의 퀘스트 미리보기에 현재 능력치/남은 능력치 표기

--- Assets/QuestPreview.cs
+++ Assets/QuestPreview.cs
@@ -22,7 +22,7 @@ public class QuestPreview : MonoBehaviour
             {
                 if (quest.req[i] > 0)
                 {
-                    stringBuilder.AppendLine($"<color={(GameData.studyExp[i] >= quest.req[i] ? "#008000" : "red")}>{Util.subjectName[i]} {quest.req[i]} 이상</color>");
+                    stringBuilder.AppendLine($"<color={(GameData.studyExp[i] >= quest.req[i] ? "#008000" : "red")}>{Util.subjectName[i]} {quest.req[i]} 이상 ({GameData.studyExp[i]}{(GameData.studyExp[i] >= quest.req[i] ? "" : $", {quest.req[i] - GameData.studyExp[i]} 남음")})</color>");
                 }
             }
             stringBuilder.Append("\n");
